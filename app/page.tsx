4636bcb4893a678de09442a6ec795cdbb62e3445
biancofla <flavio.biancospino+github@gmail.com>
2025-05-09 11:19:45 +0200

    refactor: use Game dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import { Loading } from "@/app/_components/loading";
 import { Welcome } from "@/app/_components/welcome";
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
 import { useEffect } from "react";
-import { Game } from "@/app/_components/game";
+import dynamic from "next/dynamic";
+
+const Game = dynamic(() => import("@/app/_components/game").then((mod) => mod.Game), {
+  ssr: false,
+});
 
 export default function App() {
   const { context, isFrameReady, setFrameReady } = useMiniKit();
