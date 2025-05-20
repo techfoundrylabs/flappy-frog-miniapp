ae2eca5e3d56e2cfc43a46b2db0fb7e46791c119
pf55351 <francesco.primerano79@gmail.com>
2025-05-20 11:57:12 +0200

    Add new hook to handle chain

---
+++ hooks/use-chain.ts
@@ -0,0 +1,22 @@
+import { IS_MAINNET } from "@/config/constants";
+import { base, baseSepolia } from "wagmi/chains";
+import { useChainId, useSwitchChain } from "wagmi";
+import { useState } from "react";
+
+export const useChain = () => {
+  const chain = IS_MAINNET ? base : baseSepolia;
+  const targetChain = chain.id;
+  const [chainUpdated, setChainUpdated] = useState(false);
+  const chainName = chain.name;
+  const { switchChain } = useSwitchChain();
+  const currentChain = useChainId();
+
+  const updateChain = () => {
+    if (targetChain !== currentChain && !chainUpdated) {
+      switchChain({ chainId: targetChain });
+      setChainUpdated(true);
+    }
+  };
+
+  return { chainName, chainId: targetChain, updateChain };
+};
