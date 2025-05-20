aa5e7fed232e65f5e46ed47c01efaa816e6f9bf2
pf55351 <francesco.primerano79@gmail.com>
2025-05-20 12:11:25 +0200

    Update welcome

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -16,28 +16,28 @@ export const Welcome = () => {
       <WelcomeHeader />
       <Loading className={!isImgLoaded ? "flex" : "hidden"} />
       <div
-        className={`${isImgLoaded ? "flex" : "hidden"} w-full flex-col md:flex-row md:p-12 gap-12`}
+        className={`${isImgLoaded ? "flex" : "hidden"} w-full flex-col lg:flex-row lg:p-12 gap-12`}
       >
-        <div className="flex items-center justify-center  z-10 md:w-2/3">
+        <div className="flex items-center justify-center  z-10 lg:w-2/3">
           <Image
             src={FlappyFrogPoster}
             alt="Flappy Frog"
-            className="h-[550px] w-[450px] shadow-lg shadow-violet-500 rounded-2xl"
+            className="h-[550px] w-[600px] shadow-lg shadow-violet-500 rounded-2xl"
             priority={true}
             onLoad={() => setIsImgLoaded(true)}
           />
         </div>
 
         <div className="mt-12 flex w-full flex-col gap-y-4 items-center justify-center text-[#EA7B00]">
-          <div className="hidden md:flex flex-col gap-y-2">
-            <h1 className="text-center text-lg md:text-5xl font-semibold">
+          <div className="hidden lg:flex flex-col gap-y-2">
+            <h1 className="text-center text-lg lg:text-5xl font-semibold">
               Welcome to
             </h1>
-            <h1 className="text-lg md:text-5xl text-center font-semibold">
+            <h1 className="text-lg lg:text-5xl text-center font-semibold">
               Flappy Frog
             </h1>
           </div>
-          <p className="text-sm md:text-[16px] font-light px-6 md:px-24  md:text-center text-white/80">
+          <p className="text-sm lg:text-[16px] font-light px-6 lg:px-24  lg:text-center text-white/80">
             Guide our BaseFroggy hero through a gauntlet of pipes, collecting
             points as you soar through the obstacles. Climb your way to the top
             of the leaderboard, challenge your friends, and prove your skill.
@@ -50,7 +50,7 @@ export const Welcome = () => {
             className="my-4 bg-violet-600 w-64 text-sm rounded-md self-center  p-2 text-white "
           >
             <div className="flex flex-row items-center gap-x-2">
-              <Image src={FarcasterLogo} alt="" className={"w-8 md:w-12"} />
+              <Image src={FarcasterLogo} alt="" className={"w-8 lg:w-12"} />
               Start Playing
             </div>
           </Link>
