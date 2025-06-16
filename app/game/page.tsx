f09ee02c4cc778351e36eb2619f4f75573ec2a5c
pf55351 <francesco.primerano79@gmail.com>
2025-06-16 13:27:36 +0200

    Add new page game

---
+++ app/game/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Loading } from "@/components/loading";
+import { useDepositIntoTreasury } from "@/hooks/use-smart-contracts";
+import { useMiniApp } from "@/providers/mini-app-provider";
+
+import dynamic from "next/dynamic";
+
+const FlappyFrog = dynamic(
+  () => import("@/components/flappy-frog").then((mod) => mod.FlappyFrog),
+  {
+    ssr: false,
+    loading: () => <Loading />,
+  },
+);
+
+export default function App() {
+  const { isFrameReady, fid, userName } = useMiniApp();
+
+  const { handlePayGame } = useDepositIntoTreasury();
+  if (!isFrameReady || !fid || !userName) return <Loading />;
+
+  return <FlappyFrog fid={fid} displayName={userName} pay={handlePayGame} />;
+}
