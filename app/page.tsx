e5c28a49643f0aea7611644fbf7bbdfa2e6bb406
pf55351 <francesco.primerano79@gmail.com>
2025-06-17 08:27:31 +0200

    Add home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-import { HowToPlay } from "@/components/how-to-play";
-import { BaseLayout } from "@/components/menu/base-layout";
-import { useRouter } from "next/navigation";
+import { Home } from "@/components/home/home";
 
-const InfoPage = () => {
-  const router = useRouter();
-
-  const handlePlayBtn = async () => {
-    await router.push("/game");
-  };
-
-  return (
-    <BaseLayout title="How to play" className="py-8">
-      <HowToPlay />
-      <button
-        className="my-2 btn btn-success text-white text-[12px]"
-        onClick={handlePlayBtn}
-      >
-        Ready to play...
-      </button>
-    </BaseLayout>
-  );
+const Index = () => {
+  return <Home />;
 };
 
-export default InfoPage;
+export default Index;
---
+++ components/home/home.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { HowToPlay } from "@/components/how-to-play";
+import { BaseLayout } from "@/components/menu/base-layout";
+import { useRouter } from "next/navigation";
+
+export const Home = () => {
+  const router = useRouter();
+
+  const handlePlayBtn = async () => {
+    await router.push("/game");
+  };
+  return (
+    <BaseLayout title="How to play" className="py-8">
+      <HowToPlay />
+      <button
+        className="my-2 btn btn-success text-white text-[12px]"
+        onClick={handlePlayBtn}
+      >
+        Ready to play...
+      </button>
+    </BaseLayout>
+  );
+};
