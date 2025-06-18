3a6f31f1a1ce2ce636c89aa1c46477ea0e3e437c
pf55351 <francesco.primerano79@gmail.com>
2025-06-18 13:41:29 +0200

    update the url

--- lib/event-bus/event-actions.ts
+++ lib/event-bus/event-actions.ts
@@ -4,7 +4,7 @@ import sdk from "@farcaster/frame-sdk";
 export const shareCast = async (score: number) => {
   try {
     await sdk.actions.composeCast({
-      text: `Check out this game! My score is ${score}.\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
+      text: `Check out this game!\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
       embeds: [`${APP_URL}/cast`],
     });
   } catch (error) {
