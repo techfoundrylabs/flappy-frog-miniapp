9c14620652aea2fe2adc1d6ac781e0182967fdea
techfoundrylabs <techfoundrylabsxyz@gmail.com>
2025-06-19 15:07:03 +0200

    Update README.md

--- lib/event-bus/event-actions.ts
+++ lib/event-bus/event-actions.ts
@@ -4,7 +4,7 @@ import sdk from "@farcaster/frame-sdk";
 export const shareCast = async (score: number, rank: number=0) => {
   try {
     const addtionalText =
-      !!rank && rank > 0 ? ` and my rank is ${rank + 1}` : "";
+      !!rank && rank > 0 ? ` and my rank is ${rank}` : "";
     await sdk.actions.composeCast({
       text: `Check out this game! My score is ${score}${addtionalText}.\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
       embeds: [`${APP_URL}/cast`],
