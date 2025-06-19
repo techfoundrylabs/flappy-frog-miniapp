ac783b3cf04c351d3abeaba02394f6368cd58500
pf55351 <francesco.primerano79@gmail.com>
2025-06-19 14:47:19 +0200

    add addtional text

--- lib/event-bus/event-actions.ts
+++ lib/event-bus/event-actions.ts
@@ -1,10 +1,11 @@
 import { APP_URL } from "@/config/constants";
 import sdk from "@farcaster/frame-sdk";
 
-export const shareCast = async (score: number) => {
+export const shareCast = async (score: number, rank?: number) => {
   try {
+    const addtionalText = rank ? ` and my rank is ${rank}` : "";
     await sdk.actions.composeCast({
-      text: `Check out this game! My score is ${score}\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
+      text: `Check out this game! My score is ${score}${addtionalText}.\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
       embeds: [`${APP_URL}/cast`],
     });
   } catch (error) {
