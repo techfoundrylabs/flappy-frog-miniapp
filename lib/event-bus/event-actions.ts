5617cf5bd5efe6af75d20025671316f295eb6f0c
pf55351 <francesco.primerano79@gmail.com>
2025-06-19 14:56:27 +0200

    fix rank

--- lib/event-bus/event-actions.ts
+++ lib/event-bus/event-actions.ts
@@ -3,7 +3,7 @@ import sdk from "@farcaster/frame-sdk";
 
 export const shareCast = async (score: number, rank?: number) => {
   try {
-    const addtionalText = rank ? ` and my rank is ${rank}` : "";
+    const addtionalText = rank ? ` and my rank is ${rank+1}` : "";
     await sdk.actions.composeCast({
       text: `Check out this game! My score is ${score}${addtionalText}.\nWould you like to try to beat this score and try to win the treasury pool?.\nCome on, press the button and play`,
       embeds: [`${APP_URL}/cast`],
