efda5c483fb1a0480bb4ce48590e2b078e1a3a28
pf55351 <francesco.primerano79@gmail.com>
2025-06-22 23:22:33 +0200

    fix stats

--- components/user-info/user-stats.tsx
+++ components/user-info/user-stats.tsx
@@ -9,7 +9,7 @@ interface UserStatsProps {
 
 export const UserStats = ({ score, rank, attempts }: UserStatsProps) => {
   const realScore = score ?? 0;
-  const realRank = rank ? rank + 1 : 0;
+  const realRank = !!rank ? rank + 1 : -1;
   const realAttempts = attempts ?? 0;
   return (
     <div className="backdrop-blur-md bg-white/10 rounded-2xl p-4 border border-white/20">
@@ -23,8 +23,11 @@ export const UserStats = ({ score, rank, attempts }: UserStatsProps) => {
               <p className="text-lg  text-yellow-400">{realScore}</p>
               <p className="text-blue-200 text-[10px]">Current Score</p>
             </div>
+
             <button
-              onClick={() => shareCast(realScore, realRank)}
+              onClick={() =>
+                shareCast(realScore, realRank > 0 ? realRank : undefined)
+              }
               className="bg-blue-500/50 hover:bg-blue-500/70 text-white p-2 rounded-lg transition-colors"
               title="Share Current Score"
             >
@@ -36,7 +39,9 @@ export const UserStats = ({ score, rank, attempts }: UserStatsProps) => {
         {/* Current Rank and Attempts Left */}
         <div className="grid grid-cols-2 gap-4">
           <div className="text-center bg-purple-500/20 rounded-xl p-4 border border-purple-400/30">
-            <p className="text-lg  text-purple-400">#{realRank}</p>
+            <p className="text-lg  text-purple-400">
+              {realRank > 0 ? `#${realRank}` : "-"}
+            </p>
             <p className="text-blue-200 text-[10px]">Current Rank</p>
           </div>
           <div className="text-center bg-yellow-500/20 rounded-xl p-4 border border-yellow-400/30">
